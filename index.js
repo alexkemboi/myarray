2c6a6b393773bb69420599375b4e27164d53dff9
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-02-05 16:41:29 +0300

    used filter method

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ myArray.push(7);
 myArray.pop();
 myArray.shift();
 console.log(myArray);
+let evenValues = myArray.filter((number) => number % 2 == 0);
+console.log(evenValues);
+let students = [
+  {
+    name: "John",
+    age: 20,
+    score: 90,
+  },
+  {
+    name: "Jane",
+    age: 21,
+    score: 80,
+  },
+  {
+    name: "Mike",
+    age: 2,
+    score: 70,
+  },
+];
+let newStudents = students.sort((a, b) => a.age - b.age);
+console.log(newStudents);
